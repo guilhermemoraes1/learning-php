bfb3aed2649026596eeb7ec13607e2707abe1ea2
Guilherme Moraes <guilherme.morais.54540218@gmail.com>
2024-04-18 09:33:20 -0300

    change some code from challenge 7

--- challenges/d07/index.php
+++ challenges/d07/index.php
@@ -8,15 +8,15 @@
 </head>
 <body>
     <?php 
-        $money = $_GET["v1"] ?? 1380;
-        $money = (int) $money;
+        $min = 1_412;
+        $money = $_GET["v1"] ?? $min;
     ?>
     <main>  
         <h1>Informe seu salário</h1>
         <form action="<?= $_SERVER["PHP_SELF"] ?>" method="get">
             <label for="v1">Salário (R$)</label>
-            <input type="number" name="v1" id="idv1" min="1380" value="<?=$money?>">
-            <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
+            <input type="number" name="v1" id="idv1" min="<?=$min?>" value="<?=$money?>">
+            <p>Considerando o salário mínimo de <strong>R$<?=number_format($min, 2, ",", ".")?></strong></p>
             <input type="submit" value="Calcular">
         </form>
     </main>
@@ -24,8 +24,8 @@
         <h2>Resultado final</h2>
 
         <?php
-            $quant = intdiv($money, 1380);
-            $rest = $money - ($quant * 1380);
+            $quant = intdiv($money, $min);
+            $rest = $money % $min;
             echo "Quem recebe um salário de R$" . number_format($money, 2, ",", ".") . " ganha <strong>" . number_format($quant, 0) . " salários mínimos </strong> + R$ " . number_format($rest, 2, ",", ".") . ".";
         ?>
     </section>
